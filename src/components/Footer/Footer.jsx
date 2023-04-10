647ee804e794c55dc23a730a1453ee6e26677cf2
schouppeyoran <schouppey@gmail.com>
2023-04-10 19:45:19 +0200

    develop footer CTA

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { images } from '../../constants';
 import './footer.css';
+
+const newsletterTagline = 'Stay up to speed on the latest';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section id='footer' className='app__footer'>
+      <div className="app__footer-newsletter">
+        <div className="app__footer-newsletter-img-left">
+          <img src={images.polestarHatch3} alt="" />
+        </div>
+        <div className="app__footer-newsletter-content">
+          <h2>{newsletterTagline}</h2>
+          <div className="app__footer-newsletter-content-input">
+            <input type="email" placeholder='Enter your email'/>
+            <button>Sign up</button>
+          </div>
+          <p>By clicking 'Sign up' you are confirming that you agree with our <a href="#footer">Terms and Conditions</a>.</p>
+        </div>
+        <div className="app__footer-newsletter-img-right">
+          <img src={images.polestarHatch3} alt="" />
+        </div>
+      </div>
+      <div className="app__footer-content">
+
+      </div>
+    </section>
   )
 }
 
